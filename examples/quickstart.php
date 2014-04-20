e0eb66043f8a7e02d2d102941c2d09eb4457ffd9
Codeliner <kontakt@codeliner.ws>
2014-04-20 22:35:09 +0200

    Add Quickstart

--- examples/quickstart.php
+++ examples/quickstart.php
@@ -1,31 +1,13 @@
-ProophEventStore
-===================
-PHP 5.4+ EventSourcing library.
-
-[![Build Status](https://travis-ci.org/prooph/event-store.svg?branch=master)](https://travis-ci.org/prooph/event-store)
-
-##Features
-- Event driven library [in progress]
-- Full DDD support [in progress]
-- Collection based repositories [in progress]
-- Support for ValueObjects as AggregateRoot identifiers [done]
-- Transaction handling [done]
-- Optional snapshot strategies [not started]
-- Optional concurrent logging [not started]
-- Link your own event dispatcher and/or connect ProophServiceBus [not started]
-- Replay of all events [not started]
-- FeatureManager to ease customization [not started]
-- Changeable storage adapter like
--- ZF2 Tablegateway [in progress]
--- doctrine dbal [not started]
--- mongoDB [not started]
--- geteventstore client [not started]
-
-##ZF2 insight
-The ProophEventStore is based on ZF2 components which offer a lot of customization options.
-
-##Quick start
-```php
+<?php
+/*
+ * This file is part of the codeliner/event-store.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 20.04.14 - 21:44
+ */
 namespace {
     require_once '../vendor/autoload.php';
 }
@@ -230,14 +212,3 @@ namespace {
     //Event My\Model\UserNameChanged was persisted during last transaction
     //The related Aggregate is dda1f14e-0b27-455f-ac4e-fc0043fe8455 and the payload of the event is {"username":"Alex"}
 }
-```
-
-##Contribution
-coming soon ...
-
-##Wiki
-coming soon ...
-
-
-
-
