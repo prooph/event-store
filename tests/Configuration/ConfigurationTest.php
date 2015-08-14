facb9c52944232b706e1cbc7df72cbe1cb8413cf
Alexander Miertsch <alexander.miertsch@sixt.com>
2015-08-14 09:21:52 +0200

    Fix file headers and line endings

--- tests/Configuration/ConfigurationTest.php
+++ tests/Configuration/ConfigurationTest.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * This file is part of VehicleManagement
- * Date: 8/13/15 - 10:56 PM
- * (c) Sixt GmbH & Co. Autovermietung KG
+ * This file is part of the prooph/event-store.
+ * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Prooph\EventStoreTest\Configuration;
 
@@ -374,4 +376,3 @@ final class ConfigurationTest extends TestCase
         ], $adapter->getInjectedOptions());
     }
 }
-
--- tests/Mock/AdapterMock.php
+++ tests/Mock/AdapterMock.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * This file is part of VehicleManagement
- * Date: 8/13/15 - 11:34 PM
- * (c) Sixt GmbH & Co. Autovermietung KG
+ * This file is part of the prooph/event-store.
+ * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 namespace Prooph\EventStoreTest\Mock;
 
@@ -70,4 +72,5 @@ final class AdapterMock implements Adapter
         throw new \BadMethodCallException(__CLASS__ . '::' . __METHOD__);
     }
 }
+
  
