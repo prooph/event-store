2faa98a7de0300fb2338e240377adb19bc571002
Alexander Miertsch <alexander.miertsch@sixt.com>
2015-08-22 20:52:34 +0200

    Update copyright

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of the prooph/event-store package.
- * (c) Alexander Miertsch <contact@prooph.de>
+ * (c) 2014 - 2015 prooph software GmbH <contact@prooph.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
