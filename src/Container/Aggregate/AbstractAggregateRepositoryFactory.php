c2345b75057b8bcb10daeb517967f96f8752069f
Sandro Keil <github@sandro-keil.de>
2016-03-07 22:56:19 +0100

    update factories to interop-config 1.0, add static factory support

--- src/Container/Aggregate/AbstractAggregateRepositoryFactory.php
+++ src/Container/Aggregate/AbstractAggregateRepositoryFactory.php
@@ -10,83 +10,18 @@
  */
 namespace Prooph\EventStore\Container\Aggregate;
 
-use Interop\Config\ConfigurationTrait;
-use Interop\Config\RequiresContainerId;
-use Interop\Config\RequiresMandatoryOptions;
-use Interop\Container\ContainerInterface;
-use Prooph\EventStore\Aggregate\AggregateRepository;
-use Prooph\EventStore\Aggregate\AggregateType;
-use Prooph\EventStore\EventStore;
-use Prooph\EventStore\Exception\ConfigurationException;
-use Prooph\EventStore\Stream\StreamName;
-
 /**
- * Class AbstractAggregateRepositoryFactory
- *
- * @package Prooph\EventStore\Container
+ * @deprecated Use AggregateRepositoryFactory, will be removed in next major version
  */
-abstract class AbstractAggregateRepositoryFactory implements RequiresContainerId, RequiresMandatoryOptions
+abstract class AbstractAggregateRepositoryFactory extends AggregateRepositoryFactory
 {
-    use ConfigurationTrait;
-
-    /**
-     * @param ContainerInterface $container
-     * @throws ConfigurationException
-     * @return AggregateRepository
-     */
-    public function __invoke(ContainerInterface $container)
+    public function __construct()
     {
-        $config = $container->get('config');
-        $config = $this->options($config);
-
-        $repositoryClass = $config['repository_class'];
-
-        if (! class_exists($repositoryClass)) {
-            throw ConfigurationException::configurationError(sprintf('Repository class %s cannot be found', $repositoryClass));
-        }
-
-        if (! is_subclass_of($repositoryClass, AggregateRepository::class)) {
-            throw ConfigurationException::configurationError(sprintf('Repository class %s must be a sub class of %s', $repositoryClass, AggregateRepository::class));
-        }
-
-        $eventStore = $container->get(EventStore::class);
-        $aggregateType = AggregateType::fromAggregateRootClass($config['aggregate_type']);
-        $aggregateTranslator = $container->get($config['aggregate_translator']);
-
-        $snapshotStore = isset($config['snapshot_store'])? $container->get($config['snapshot_store']) : null;
-
-        $streamName = isset($config['stream_name'])? new StreamName($config['stream_name']) : null;
-
-        $oneStreamPerAggregate = isset($config['one_stream_per_aggregate'])? (bool)$config['one_stream_per_aggregate'] : false;
-
-        return new $repositoryClass($eventStore, $aggregateType, $aggregateTranslator, $snapshotStore, $streamName, $oneStreamPerAggregate);
+        parent::__construct($this->containerId());
     }
 
     /**
      * @inheritdoc
      */
-    public function vendorName()
-    {
-        return 'prooph';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function packageName()
-    {
-        return 'event_store';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function mandatoryOptions()
-    {
-        return [
-            'repository_class',
-            'aggregate_type',
-            'aggregate_translator',
-        ];
-    }
+    abstract public function containerId();
 }
