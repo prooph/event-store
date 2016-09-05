52ca8c7cc966152e2d17da20e73213d5c7e221f4
prolic <saschaprolic@googlemail.com>
2016-09-05 19:46:04 +1200

    update license in file headers

--- src/Adapter/Feature/CanHandleTransaction.php
+++ src/Adapter/Feature/CanHandleTransaction.php
@@ -1,7 +1,8 @@
 <?php
-/*
- * This file is part of the prooph/event-store package.
- * (c) 2014 - 2015 prooph software GmbH <contact@prooph.de>
+/**
+ * This file is part of the prooph/service-bus.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
