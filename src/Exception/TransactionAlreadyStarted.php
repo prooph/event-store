555bd11989dd14b238bc4915a717e312c82cc370
prolic <saschaprolic@googlemail.com>
2016-11-11 15:10:21 +0800

    add AbstractCanControlTransactionActionEventEmitterAwareEventStore add some exception classes

---
+++ src/Exception/TransactionAlreadyStarted.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the prooph/event-store.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore\Exception;
+
+class TransactionAlreadyStarted extends RuntimeException
+{
+    protected $message = 'The transaction has already been started.';
+}
