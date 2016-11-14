19a7d7912905a35445a3208be90eba7b5fce4e45
prolic <saschaprolic@googlemail.com>
2016-11-14 23:03:29 +0800

    fix access level

--- src/InMemoryEventStore.php
+++ src/InMemoryEventStore.php
@@ -29,7 +29,7 @@ final class InMemoryEventStore extends AbstractActionEventEmitterAwareEventStore
     /**
      * @var ActionEventEmitter
      */
-    private $actionEventEmitter;
+    protected $actionEventEmitter;
 
     public function __construct(ActionEventEmitter $actionEventEmitter)
     {
