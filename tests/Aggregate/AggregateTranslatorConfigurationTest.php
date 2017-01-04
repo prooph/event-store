ef425d5d45769d7c9ada24a152772dd2e3c2a01e
dropdevcoding <peter.hildenbrand@gmail.com>
2017-01-04 22:00:28 +0100

    udated docheaders

--- tests/Aggregate/AggregateTranslatorConfigurationTest.php
+++ tests/Aggregate/AggregateTranslatorConfigurationTest.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * This file is part of the prooph/service-bus.
- * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
- * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * This file is part of the prooph/event-store.
+ * (c) 2014-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace ProophTest\EventStore\Aggregate;
 
 use PHPUnit_Framework_TestCase as TestCase;
