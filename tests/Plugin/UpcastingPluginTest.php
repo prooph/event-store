287c5f836d91c284c054c9c595837f7b92be409e
prolic <saschaprolic@googlemail.com>
2017-02-08 15:27:34 +0800

    add upcasting plugin tests

--- tests/Plugin/UpcastingPluginTest.php
+++ tests/Plugin/UpcastingPluginTest.php
@@ -12,32 +12,47 @@ declare(strict_types=1);
 
 namespace ProophTest\EventStore\Plugin;
 
+use Prooph\Common\Messaging\Message;
+use Prooph\EventStore\Exception\StreamNotFound;
+use Prooph\EventStore\Plugin\UpcastingPlugin;
 use Prooph\EventStore\Stream;
 use Prooph\EventStore\StreamName;
+use Prooph\EventStore\Upcasting\NoOpEventUpcaster;
+use Prooph\EventStore\Upcasting\SingleEventUpcaster;
 use ProophTest\EventStore\ActionEventEmitterEventStoreTestCase;
 use ProophTest\EventStore\Mock\EventLoggerPlugin;
 use ProophTest\EventStore\Mock\UserCreated;
+use ProophTest\EventStore\Mock\UsernameChanged;
 use Zend\ServiceManager\ServiceManager;
 
-class PluginManagerTest extends ActionEventEmitterEventStoreTestCase
+class UpcastingPluginTest extends ActionEventEmitterEventStoreTestCase
 {
     /**
      * @test
      */
-    public function an_invokable_plugin_is_loaded_by_plugin_manager_and_attached_to_event_store_by_configuration(): void
+    public function it_attaches_to_event_store(): void
     {
-        $pluginManager = new ServiceManager([
-            'invokables' => [
-                'eventlogger' => EventLoggerPlugin::class,
-            ],
-        ]);
+        $upcaster = new class extends SingleEventUpcaster {
+            protected function canUpcast(Message $message): bool
+            {
+                return true;
+            }
 
-        $logger = $pluginManager->get('eventlogger');
-        $logger->attachToEventStore($this->eventStore);
+            protected function doUpcast(Message $message): array
+            {
+                return [$message->withAddedMetadata('key', 'value')];
+            }
+
+        };
+
+        $plugin = new UpcastingPlugin($upcaster);
+        $plugin->attachToEventStore($this->eventStore);
+
+        $streamName = new StreamName('user');
 
         $this->eventStore->create(
             new Stream(
-                new StreamName('user'),
+                $streamName,
                 new \ArrayIterator([
                     UserCreated::with(
                         [
@@ -46,12 +61,43 @@ class PluginManagerTest extends ActionEventEmitterEventStoreTestCase
                         ],
                         1
                     ),
+                    UsernameChanged::with(
+                        [
+                            'name' => 'Sascha',
+                        ],
+                        2
+                    ),
                 ])
             )
         );
 
-        $loggedStreamEvents = $pluginManager->get('eventlogger')->getLoggedStreamEvents();
+        $iterator = $this->eventStore->load($streamName);
 
-        $this->assertEquals(1, count($loggedStreamEvents));
+        $this->assertTrue($iterator->valid());
+        $this->assertEquals(['key' => 'value', '_aggregate_version' => 1], $iterator->current()->metadata());
+        $iterator->next();
+        $this->assertTrue($iterator->valid());
+        $this->assertEquals(['key' => 'value', '_aggregate_version' => 2], $iterator->current()->metadata());
+
+        $iterator = $this->eventStore->loadReverse($streamName);
+
+        $this->assertTrue($iterator->valid());
+        $this->assertEquals(['key' => 'value', '_aggregate_version' => 2], $iterator->current()->metadata());
+        $iterator->next();
+        $this->assertTrue($iterator->valid());
+        $this->assertEquals(['key' => 'value', '_aggregate_version' => 1], $iterator->current()->metadata());
+    }
+
+    /**
+     * @test
+     */
+    public function it_ignores_when_no_iterator_in_result(): void
+    {
+        $this->expectException(StreamNotFound::class);
+
+        $plugin = new UpcastingPlugin(new NoOpEventUpcaster());
+        $plugin->attachToEventStore($this->eventStore);
+
+        $this->eventStore->load(new StreamName('user'));
     }
 }
