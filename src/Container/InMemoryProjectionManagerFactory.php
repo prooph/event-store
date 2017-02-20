e827dd887179b0cd17b3074cda0410484d669869
prolic <saschaprolic@googlemail.com>
2017-02-20 21:07:07 +0800

    add InMemoryProjectionManagerFactory

---
+++ src/Container/InMemoryProjectionManagerFactory.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This file is part of the prooph/event-store.
+ * (c) 2014-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore\Container;
+
+use Interop\Config\ConfigurationTrait;
+use Interop\Config\RequiresConfig;
+use Interop\Config\RequiresConfigId;
+use Interop\Config\RequiresMandatoryOptions;
+use Prooph\EventStore\Exception\ConfigurationException;
+use Prooph\EventStore\Exception\InvalidArgumentException;
+use Prooph\EventStore\Projection\InMemoryProjectionManager;
+use Psr\Container\ContainerInterface;
+
+final class InMemoryProjectionManagerFactory implements
+    RequiresConfig,
+    RequiresConfigId,
+    RequiresMandatoryOptions
+{
+    use ConfigurationTrait;
+
+    /**
+     * @var string
+     */
+    private $configId;
+
+    /**
+     * Creates a new instance from a specified config, specifically meant to be used as static factory.
+     *
+     * In case you want to use another config key than provided by the factories, you can add the following factory to
+     * your config:
+     *
+     * <code>
+     * <?php
+     * return [
+     *     InMemoryProjectionManager::class => [InMemoryProjectionManager::class, 'service_name'],
+     * ];
+     * </code>
+     *
+     * @throws InvalidArgumentException
+     */
+    public static function __callStatic(string $name, array $arguments): InMemoryProjectionManager
+    {
+        if (! isset($arguments[0]) || ! $arguments[0] instanceof ContainerInterface) {
+            throw new InvalidArgumentException(
+                sprintf('The first argument must be of type %s', ContainerInterface::class)
+            );
+        }
+
+        return (new static($name))->__invoke($arguments[0]);
+    }
+
+    public function __construct(string $configId = 'default')
+    {
+        $this->configId = $configId;
+    }
+
+    /**
+     * @throws ConfigurationException
+     */
+    public function __invoke(ContainerInterface $container): InMemoryProjectionManager
+    {
+        $config = $container->get('config');
+        $config = $this->options($config, $this->configId);
+
+        $eventStore = $container->get($config['event_store']);
+
+        return new InMemoryProjectionManager($eventStore);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dimensions(): iterable
+    {
+        return ['prooph', 'projection_manager'];
+    }
+
+    public function mandatoryOptions(): iterable
+    {
+        return [
+            'event_store',
+        ];
+    }
+}
