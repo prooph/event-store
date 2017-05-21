037df65cbbcaf01d7dea88042627b4052a65efb8
prolic <saschaprolic@googlemail.com>
2017-05-21 18:42:00 +0800

    add message property matching
    resolves https://github.com/prooph/event-store/issues/284

--- src/Metadata/MetadataMatcher.php
+++ src/Metadata/MetadataMatcher.php
@@ -23,12 +23,26 @@ class MetadataMatcher
         return $this->data;
     }
 
-    public function withMetadataMatch(string $field, Operator $operator, $value): MetadataMatcher
-    {
+    public function withMetadataMatch(
+        string $field,
+        Operator $operator,
+        $value,
+        FieldType $fieldType = null
+    ): MetadataMatcher {
         $this->validateValue($value);
 
+        if (null === $fieldType) {
+            $fieldType = FieldType::METADATA();
+        }
+
+        if ($fieldType->is(FieldType::MESSAGE_PROPERTY())
+            && ! in_array($field, ['uuid', 'createdAt', 'created_at', 'messageName', 'message_name'], true)
+        ) {
+            throw new InvalidArgumentException(sprintf('Invalid message property "%s" given', $field));
+        }
+
         $self = clone $this;
-        $self->data[] = ['field' => $field, 'operator' => $operator, 'value' => $value];
+        $self->data[] = ['field' => $field, 'operator' => $operator, 'value' => $value, 'fieldType' => $fieldType];
 
         return $self;
     }
