6a053725e6b60e1392d3dfdd70dc5e40733a76a1
Tomáš Čížek <tomascizek@gmail.com>
2017-10-26 19:28:54 +0200

    getting rid of abstractions, introducing 'transactional' flag for InMemoryEventStoreFactory

--- src/Container/InMemoryEventStoreFactory.php
+++ src/Container/InMemoryEventStoreFactory.php
@@ -12,31 +12,193 @@ declare(strict_types=1);
 
 namespace Prooph\EventStore\Container;
 
+use Interop\Config\ConfigurationTrait;
+use Interop\Config\ProvidesDefaultOptions;
+use Interop\Config\RequiresConfig;
+use Interop\Config\RequiresConfigId;
 use Prooph\Common\Event\ActionEventEmitter;
+use Prooph\Common\Event\ProophActionEventEmitter;
 use Prooph\EventStore\ActionEventEmitterEventStore;
 use Prooph\EventStore\EventStore;
+use Prooph\EventStore\Exception\ConfigurationException;
+use Prooph\EventStore\Exception\InvalidArgumentException;
 use Prooph\EventStore\InMemoryEventStore;
+use Prooph\EventStore\Metadata\MetadataEnricher;
+use Prooph\EventStore\Metadata\MetadataEnricherAggregate;
+use Prooph\EventStore\Metadata\MetadataEnricherPlugin;
+use Prooph\EventStore\NonTransactionalInMemoryEventStore;
+use Prooph\EventStore\Plugin\Plugin;
+use Prooph\EventStore\ReadOnlyEventStore;
+use Prooph\EventStore\ReadOnlyEventStoreWrapper;
 use Prooph\EventStore\TransactionalActionEventEmitterEventStore;
 use Prooph\EventStore\TransactionalEventStore;
+use Psr\Container\ContainerInterface;
 
-final class InMemoryEventStoreFactory extends AbstractInMemoryEventStoreFactory
+final class InMemoryEventStoreFactory implements
+    ProvidesDefaultOptions,
+    RequiresConfig,
+    RequiresConfigId
 {
-    protected function getEventsForDefaultEmitter(): array
+    use ConfigurationTrait;
+
+    /**
+     * @var string
+     */
+    private $configId;
+
+    /**
+     * Creates a new instance from a specified config, specifically meant to be used as static factory.
+     * In case you want to use another config key than provided by the factories, you can add the following factory to
+     * your config:
+     * <code>
+     * <?php
+     * return [
+     *     InMemoryEventStore::class => [InMemoryEventStoreFactory::class, 'service_name'],
+     * ];
+     * </code>
+     * @throws InvalidArgumentException
+     */
+    public static function __callStatic(string $name, array $arguments): ReadOnlyEventStore
     {
-        return TransactionalActionEventEmitterEventStore::ALL_EVENTS;
+        if (! isset($arguments[0]) || ! $arguments[0] instanceof ContainerInterface) {
+            throw new InvalidArgumentException(
+                sprintf('The first argument must be of type %s', ContainerInterface::class)
+            );
+        }
+
+        return (new static($name))->__invoke($arguments[0]);
     }
 
-    protected function createEventStoreInstance(): EventStore
+    public function __construct(string $configId = 'default')
     {
-        return new InMemoryEventStore();
+        $this->configId = $configId;
     }
 
-    protected function createActionEventEmitterDecorator(
+    /**
+     * @throws ConfigurationException
+     */
+    public function __invoke(ContainerInterface $container): ReadOnlyEventStore
+    {
+        $config = $container->get('config');
+        $config = $this->options($config, $this->configId);
+
+        $eventStore = $this->createEventStore($config);
+
+        if ($config['read_only']) {
+            $eventStore = new ReadOnlyEventStoreWrapper($eventStore);
+        }
+
+        if (! $config['wrap_action_event_emitter']) {
+            return $eventStore;
+        }
+
+        if (! isset($config['event_emitter'])) {
+            $eventEmitter = new ProophActionEventEmitter($this->determineEventsForDefaultEmitter($config));
+        } else {
+            $eventEmitter = $container->get($config['event_emitter']);
+        }
+
+        $wrapper = $this->createActionEventEmitterDecorator($config, $eventStore, $eventEmitter);
+
+        foreach ($config['plugins'] as $pluginAlias) {
+            $plugin = $container->get($pluginAlias);
+
+            if (! $plugin instanceof Plugin) {
+                throw ConfigurationException::configurationError(
+                    sprintf(
+                        'Plugin %s does not implement the Plugin interface',
+                        $pluginAlias
+                    )
+                );
+            }
+
+            $plugin->attachToEventStore($wrapper);
+        }
+
+        if (count($config['metadata_enrichers']) > 0) {
+            $metadataEnrichers = [];
+
+            foreach ($config['metadata_enrichers'] as $metadataEnricherAlias) {
+                $metadataEnricher = $container->get($metadataEnricherAlias);
+
+                if (! $metadataEnricher instanceof MetadataEnricher) {
+                    throw ConfigurationException::configurationError(
+                        sprintf(
+                            'Metadata enricher %s does not implement the MetadataEnricher interface',
+                            $metadataEnricherAlias
+                        )
+                    );
+                }
+
+                $metadataEnrichers[] = $metadataEnricher;
+            }
+
+            $plugin = new MetadataEnricherPlugin(
+                new MetadataEnricherAggregate($metadataEnrichers)
+            );
+
+            $plugin->attachToEventStore($wrapper);
+        }
+
+        return $wrapper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dimensions(): iterable
+    {
+        return ['prooph', 'event_store'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultOptions(): iterable
+    {
+        return [
+            'metadata_enrichers' => [],
+            'plugins' => [],
+            'wrap_action_event_emitter' => true,
+            'transactional' => true,
+            'read_only' => false,
+        ];
+    }
+
+    private function determineEventsForDefaultEmitter(array $config): array
+    {
+        if ($this->isTransactional($config)) {
+            return TransactionalActionEventEmitterEventStore::ALL_EVENTS;
+        }
+
+        return ActionEventEmitterEventStore::ALL_EVENTS;
+    }
+
+    private function createEventStore(array $config): EventStore
+    {
+        if ($this->isTransactional($config)) {
+            return new InMemoryEventStore();
+        }
+
+        return new NonTransactionalInMemoryEventStore();
+    }
+
+    private function createActionEventEmitterDecorator(
+        array $config,
         EventStore $eventStore,
         ActionEventEmitter $actionEventEmitter
     ): ActionEventEmitterEventStore {
-        assert($eventStore instanceof TransactionalEventStore);
+        if ($this->isTransactional($config)) {
+            assert($eventStore instanceof TransactionalEventStore);
 
-        return new TransactionalActionEventEmitterEventStore($eventStore, $actionEventEmitter);
+            return new TransactionalActionEventEmitterEventStore($eventStore, $actionEventEmitter);
+        }
+
+        return new ActionEventEmitterEventStore($eventStore, $actionEventEmitter);
+    }
+
+    private function isTransactional(array $config): bool
+    {
+        return isset($config['transactional']) && $config['transactional'] === true;
     }
 }
--- src/NonTransactionalInMemoryEventStore.php
+++ src/NonTransactionalInMemoryEventStore.php
@@ -12,6 +12,469 @@ declare(strict_types=1);
 
 namespace Prooph\EventStore;
 
-final class NonTransactionalInMemoryEventStore extends AbstractInMemoryEventStore
+use ArrayIterator;
+use EmptyIterator;
+use Iterator;
+use Prooph\Common\Messaging\Message;
+use Prooph\EventStore\Exception\StreamExistsAlready;
+use Prooph\EventStore\Exception\StreamNotFound;
+use Prooph\EventStore\Metadata\FieldType;
+use Prooph\EventStore\Metadata\MetadataMatcher;
+use Prooph\EventStore\Metadata\Operator;
+use Prooph\EventStore\Util\Assertion;
+
+final class NonTransactionalInMemoryEventStore implements EventStore
 {
+    /**
+     * @var array
+     */
+    protected $streams = [];
+
+    /**
+     * @var array
+     */
+    protected $cachedStreams = [];
+
+    public function create(Stream $stream): void
+    {
+        $streamName = $stream->streamName();
+        $streamNameString = $streamName->toString();
+
+        if (
+            isset($this->streams[$streamNameString])
+            || isset($this->cachedStreams[$streamNameString])
+        ) {
+            throw StreamExistsAlready::with($streamName);
+        }
+
+        $this->streams[$streamNameString]['events'] = iterator_to_array($stream->streamEvents());
+        $this->streams[$streamNameString]['metadata'] = $stream->metadata();
+    }
+
+    public function appendTo(StreamName $streamName, Iterator $streamEvents): void
+    {
+        $streamNameString = $streamName->toString();
+
+        if (
+            ! isset($this->streams[$streamNameString])
+            && ! isset($this->cachedStreams[$streamNameString])
+        ) {
+            throw StreamNotFound::with($streamName);
+        }
+
+        foreach ($streamEvents as $streamEvent) {
+            $this->streams[$streamNameString]['events'][] = $streamEvent;
+        }
+    }
+
+    public function load(
+        StreamName $streamName,
+        int $fromNumber = 1,
+        int $count = null,
+        MetadataMatcher $metadataMatcher = null
+    ): Iterator {
+        Assertion::greaterOrEqualThan($fromNumber, 1);
+        Assertion::nullOrGreaterOrEqualThan($count, 1);
+
+        if (! isset($this->streams[$streamName->toString()])) {
+            throw StreamNotFound::with($streamName);
+        }
+
+        if (null === $metadataMatcher) {
+            $metadataMatcher = new MetadataMatcher();
+        }
+
+        $found = 0;
+        $streamEvents = [];
+
+        foreach ($this->streams[$streamName->toString()]['events'] as $key => $streamEvent) {
+            /* @var Message $streamEvent */
+            if (
+                ($key + 1) >= $fromNumber
+                && $this->matchesMetadata($metadataMatcher, $streamEvent->metadata())
+                && $this->matchesMessagesProperty($metadataMatcher, $streamEvent)
+            ) {
+                ++$found;
+                $streamEvents[] = $streamEvent;
+
+                if ($found === $count) {
+                    break;
+                }
+            }
+        }
+
+        if (0 === $found) {
+            return new EmptyIterator();
+        }
+
+        return new ArrayIterator($streamEvents);
+    }
+
+    public function loadReverse(
+        StreamName $streamName,
+        int $fromNumber = null,
+        int $count = null,
+        MetadataMatcher $metadataMatcher = null
+    ): Iterator {
+        if (null === $fromNumber) {
+            $fromNumber = PHP_INT_MAX;
+        }
+
+        Assertion::greaterOrEqualThan($fromNumber, 1);
+        Assertion::nullOrGreaterOrEqualThan($count, 1);
+
+        if (! isset($this->streams[$streamName->toString()])) {
+            throw StreamNotFound::with($streamName);
+        }
+
+        if (null === $metadataMatcher) {
+            $metadataMatcher = new MetadataMatcher();
+        }
+
+        $found = 0;
+        $streamEvents = [];
+
+        foreach (array_reverse($this->streams[$streamName->toString()]['events'], true) as $key => $streamEvent) {
+            /* @var Message $streamEvent */
+            if (
+                ($key + 1) <= $fromNumber
+                && $this->matchesMetadata($metadataMatcher, $streamEvent->metadata())
+                && $this->matchesMessagesProperty($metadataMatcher, $streamEvent)
+            ) {
+                $streamEvents[] = $streamEvent;
+                ++$found;
+
+                if ($found === $count) {
+                    break;
+                }
+            }
+        }
+
+        if (0 === $found) {
+            return new EmptyIterator();
+        }
+
+        return new ArrayIterator($streamEvents);
+    }
+
+    public function delete(StreamName $streamName): void
+    {
+        $streamNameString = $streamName->toString();
+
+        if (isset($this->streams[$streamNameString])) {
+            unset($this->streams[$streamNameString]);
+        } else {
+            throw StreamNotFound::with($streamName);
+        }
+    }
+
+    public function hasStream(StreamName $streamName): bool
+    {
+        return isset($this->streams[$streamName->toString()]);
+    }
+
+    public function fetchStreamMetadata(StreamName $streamName): array
+    {
+        if (! isset($this->streams[$streamName->toString()])) {
+            throw StreamNotFound::with($streamName);
+        }
+
+        return $this->streams[$streamName->toString()]['metadata'];
+    }
+
+    public function updateStreamMetadata(StreamName $streamName, array $newMetadata): void
+    {
+        if (! isset($this->streams[$streamName->toString()])) {
+            throw StreamNotFound::with($streamName);
+        }
+
+        $this->streams[$streamName->toString()]['metadata'] = $newMetadata;
+    }
+
+    public function fetchStreamNames(
+        ?string $filter,
+        ?MetadataMatcher $metadataMatcher,
+        int $limit = 20,
+        int $offset = 0
+    ): array {
+        $result = [];
+
+        $skipped = 0;
+        $found = 0;
+
+        $streams = $this->streams;
+
+        if (
+            $filter
+            && array_key_exists($filter, $streams)
+            && (
+                ! $metadataMatcher
+                || $metadataMatcher && $this->matchesMetadata($metadataMatcher, $streams[$filter]['metadata'])
+            )
+        ) {
+            return [$filter];
+        }
+
+        ksort($streams);
+
+        foreach ($streams as $streamName => $data) {
+            if (null === $filter || $filter === $streamName) {
+                if ($offset > $skipped) {
+                    ++$skipped;
+                    continue;
+                }
+
+                if ($metadataMatcher && ! $this->matchesMetadata($metadataMatcher, $data['metadata'])) {
+                    continue;
+                }
+
+                $result[] = new StreamName($streamName);
+                ++$found;
+            }
+
+            if ($found === $limit) {
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    public function fetchStreamNamesRegex(
+        string $filter,
+        ?MetadataMatcher $metadataMatcher,
+        int $limit = 20,
+        int $offset = 0
+    ): array {
+        if (false === @preg_match("/$filter/", '')) {
+            throw new Exception\InvalidArgumentException('Invalid regex pattern given');
+        }
+
+        $result = [];
+
+        $found = 0;
+
+        $streams = $this->streams;
+        ksort($streams);
+
+        foreach ($streams as $streamName => $data) {
+            if (! preg_match("/$filter/", $streamName)) {
+                continue;
+            }
+
+            if ($metadataMatcher && ! $this->matchesMetadata($metadataMatcher, $data['metadata'])) {
+                continue;
+            }
+
+            $result[] = new StreamName($streamName);
+            ++$found;
+
+            if ($found === $limit) {
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    public function fetchCategoryNames(?string $filter, int $limit = 20, int $offset = 0): array
+    {
+        $result = [];
+
+        $skipped = 0;
+        $found = 0;
+
+        $categories = array_unique(
+            array_reduce(
+                array_keys($this->streams),
+                function (array $result, string $streamName): array {
+                    if (preg_match('/^(.+)-.+$/', $streamName, $matches)) {
+                        $result[] = $matches[1];
+                    }
+
+                    return $result;
+                },
+                []
+            )
+        );
+
+        if ($filter && in_array($filter, $categories, true)) {
+            return [$filter];
+        }
+
+        ksort($categories);
+
+        foreach ($categories as $category) {
+            if (null === $filter || $filter === $category) {
+                if ($offset > $skipped) {
+                    ++$skipped;
+                    continue;
+                }
+
+                $result[] = $category;
+                ++$found;
+            }
+
+            if ($found === $limit) {
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    public function fetchCategoryNamesRegex(string $filter, int $limit = 20, int $offset = 0): array
+    {
+        if (false === @preg_match("/$filter/", '')) {
+            throw new Exception\InvalidArgumentException('Invalid regex pattern given');
+        }
+
+        $result = [];
+
+        $skipped = 0;
+        $found = 0;
+
+        $categories = array_unique(
+            array_reduce(
+                array_keys($this->streams),
+                function (array $result, string $streamName): array {
+                    if (preg_match('/^(.+)-.+$/', $streamName, $matches)) {
+                        $result[] = $matches[1];
+                    }
+
+                    return $result;
+                },
+                []
+            )
+        );
+
+        ksort($categories);
+
+        foreach ($categories as $category) {
+            if (! preg_match("/$filter/", $category)) {
+                continue;
+            }
+
+            if ($offset > $skipped) {
+                ++$skipped;
+                continue;
+            }
+
+            $result[] = $category;
+            ++$found;
+
+            if ($found === $limit) {
+                break;
+            }
+        }
+
+        return $result;
+    }
+
+    private function matchesMetadata(MetadataMatcher $metadataMatcher, array $metadata): bool
+    {
+        foreach ($metadataMatcher->data() as $match) {
+            if (! FieldType::METADATA()->is($match['fieldType'])) {
+                continue;
+            }
+
+            $field = $match['field'];
+
+            if (! isset($metadata[$field])) {
+                return false;
+            }
+
+            if (! $this->match($match['operator'], $metadata[$field], $match['value'])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function matchesMessagesProperty(MetadataMatcher $metadataMatcher, Message $message): bool
+    {
+        foreach ($metadataMatcher->data() as $match) {
+            if (! FieldType::MESSAGE_PROPERTY()->is($match['fieldType'])) {
+                continue;
+            }
+
+            switch ($match['field']) {
+                case 'uuid':
+                    $value = $message->uuid()->toString();
+                    break;
+                case 'message_name':
+                case 'messageName':
+                    $value = $message->messageName();
+                    break;
+                case 'created_at':
+                case 'createdAt':
+                    $value = $message->createdAt()->format('Y-m-d\TH:i:s.u');
+                    break;
+                default:
+                    throw new \UnexpectedValueException(sprintf('Unexpected field "%s" given', $match['field']));
+            }
+
+            if (! $this->match($match['operator'], $value, $match['value'])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function match(Operator $operator, $value, $expected): bool
+    {
+        switch ($operator) {
+            case Operator::EQUALS():
+                if ($value !== $expected) {
+                    return false;
+                }
+                break;
+            case Operator::GREATER_THAN():
+                if (! ($value > $expected)) {
+                    return false;
+                }
+                break;
+            case Operator::GREATER_THAN_EQUALS():
+                if (! ($value >= $expected)) {
+                    return false;
+                }
+                break;
+            case Operator::IN():
+                if (! in_array($value, $expected, true)) {
+                    return false;
+                }
+                break;
+            case Operator::LOWER_THAN():
+                if (! ($value < $expected)) {
+                    return false;
+                }
+                break;
+            case Operator::LOWER_THAN_EQUALS():
+                if (! ($value <= $expected)) {
+                    return false;
+                }
+                break;
+            case Operator::NOT_EQUALS():
+                if ($value === $expected) {
+                    return false;
+                }
+                break;
+            case Operator::NOT_IN():
+                if (in_array($value, $expected, true)) {
+                    return false;
+                }
+                break;
+            case Operator::REGEX():
+                if (! preg_match('/' . $expected . '/', $value)) {
+                    return false;
+                }
+                break;
+            default:
+                throw new \UnexpectedValueException('Unknown operator found');
+        }
+
+        return true;
+    }
 }
