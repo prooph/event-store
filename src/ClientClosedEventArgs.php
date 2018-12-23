af69381500f5383ab4c36a271ad3ad7c5a22c98f
prolic <saschaprolic@googlemail.com>
2018-12-24 02:29:58 +0800

    move event handlers for asyncconnections here

---
+++ src/ClientClosedEventArgs.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of prooph/event-store.
+ * (c) 2014-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore;
+
+class ClientClosedEventArgs implements EventArgs
+{
+    /** @var AsyncEventStoreConnection */
+    private $connection;
+    /** @var string */
+    private $reason;
+
+    public function __construct(AsyncEventStoreConnection $connection, string $reason)
+    {
+        $this->connection = $connection;
+        $this->reason = $reason;
+    }
+
+    public function connection(): AsyncEventStoreConnection
+    {
+        return $this->connection;
+    }
+
+    public function reason(): string
+    {
+        return $this->reason;
+    }
+}
---
+++ src/ClientErrorEventArgs.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of prooph/event-store.
+ * (c) 2014-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore;
+
+use Throwable;
+
+class ClientErrorEventArgs implements EventArgs
+{
+    /** @var AsyncEventStoreConnection */
+    private $connection;
+    /** @var Throwable */
+    private $exception;
+
+    public function __construct(AsyncEventStoreConnection $connection, Throwable $exception)
+    {
+        $this->connection = $connection;
+        $this->exception = $exception;
+    }
+
+    public function connection(): AsyncEventStoreConnection
+    {
+        return $this->connection;
+    }
+
+    public function exception(): Throwable
+    {
+        return $this->exception;
+    }
+}
