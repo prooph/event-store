320cc28bef1cbd58ad41814ec3a4c9c4338a988f
prolic <saschaprolic@googlemail.com>
2018-12-23 23:26:38 +0800

    make EventStoreSubscription an abstract class

--- src/EventStoreSubscription.php
+++ src/EventStoreSubscription.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of prooph/event-store.
- * (c) 2014-2018 prooph software GmbH <contact@prooph.de>
- * (c) 2015-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * This file is part of `prooph/event-store-client`.
+ * (c) 2018-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2018-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,19 +11,56 @@
 
 declare(strict_types=1);
 
-namespace Prooph\EventStore;
+namespace Prooph\EventStoreClient;
 
-interface EventStoreSubscription
+abstract class EventStoreSubscription
 {
-    public function isSubscribedToAll(): bool;
+    /** @var bool */
+    private $isSubscribedToAll;
+    /** @var string */
+    private $streamId;
+    /** @var int */
+    private $lastCommitPosition;
+    /** @var int|null */
+    private $lastEventNumber;
 
-    public function streamId(): string;
+    public function __construct(string $streamId, int $lastCommitPosition, ?int $lastEventNumber)
+    {
+        $this->isSubscribedToAll = $streamId === '';
+        $this->streamId = $streamId;
+        $this->lastCommitPosition = $lastCommitPosition;
+        $this->lastEventNumber = $lastEventNumber;
+    }
 
-    public function lastCommitPosition(): int;
+    public function isSubscribedToAll(): bool
+    {
+        return $this->isSubscribedToAll;
+    }
 
-    public function lastEventNumber(): ?int;
+    public function streamId(): string
+    {
+        return $this->streamId;
+    }
 
-    public function close(): void;
+    public function lastCommitPosition(): int
+    {
+        return $this->lastCommitPosition;
+    }
 
-    public function unsubscribe(): void;
+    public function lastEventNumber(): ?int
+    {
+        return $this->lastEventNumber;
+    }
+
+    public function __destruct()
+    {
+        $this->unsubscribe();
+    }
+
+    public function close(): void
+    {
+        $this->unsubscribe();
+    }
+
+    abstract public function unsubscribe(): void;
 }
