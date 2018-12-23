037f4be6a445fda01822201a49b9e477b53bc5da
prolic <saschaprolic@googlemail.com>
2018-12-23 23:52:53 +0800

    fix EventStoreSubscription

--- src/EventStoreSubscription.php
+++ src/EventStoreSubscription.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of `prooph/event-store-client`.
- * (c) 2018-2018 prooph software GmbH <contact@prooph.de>
- * (c) 2018-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * This file is part of prooph/event-store.
+ * (c) 2014-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Prooph\EventStoreClient;
+namespace Prooph\EventStore;
 
 abstract class EventStoreSubscription
 {
