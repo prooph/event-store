fa207dee6b5aa32cf8c809d58a24aee563108f8a
prolic <saschaprolic@googlemail.com>
2018-12-30 20:08:52 +0800

    update WrongExpectedVersionException

--- src/Exception/WrongExpectedVersionException.php
+++ src/Exception/WrongExpectedVersionException.php
@@ -18,10 +18,16 @@ class WrongExpectedVersionException extends RuntimeException
     public static function with(
         string $stream,
         int $expectedVersion,
-        int $currentVersion
+        ?int $currentVersion = null
     ): WrongExpectedVersionException {
+        $message = 'Operation failed due to WrongExpectedVersion. Stream: \'%s\', Expected version: %d';
+
+        if (null !== $currentVersion) {
+            $message = ', Current version: %d';
+        }
+
         return new self(\sprintf(
-            'Operation failed due to WrongExpectedVersion. Stream: \'%s\', Expected version: %d, Current version: %d',
+            $message,
             $stream,
             $expectedVersion,
             $currentVersion
