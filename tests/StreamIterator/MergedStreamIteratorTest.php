f73253203f81c58f83991356c6743c2038ae2241
Bas Kamer <bas@bushbaby.nl>
2019-05-14 09:02:55 +0200

    tests

--- tests/StreamIterator/MergedStreamIteratorTest.php
+++ tests/StreamIterator/MergedStreamIteratorTest.php
@@ -25,19 +25,19 @@ class MergedStreamIteratorTest extends AbstractStreamIteratorTest
     {
         return [
             'streamA' => new InMemoryStreamIterator([
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 5, 'expected_stream_name' => 'streamA'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388510')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 7, 'expected_stream_name' => 'streamA'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388519')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 8, 'expected_stream_name' => 'streamA'], 3, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388520')),
+                1 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 5, 'expected_position' => 1, 'expected_stream_name' => 'streamA'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388510')),
+                2 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 7, 'expected_position' => 2, 'expected_stream_name' => 'streamA'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388519')),
+                3 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 8, 'expected_position' => 3, 'expected_stream_name' => 'streamA'], 3, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388520')),
             ]),
             'streamB' => new InMemoryStreamIterator([
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 1, 'expected_stream_name' => 'streamB'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388501')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 2, 'expected_stream_name' => 'streamB'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388503')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 4, 'expected_stream_name' => 'streamB'], 3, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388509')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 6, 'expected_stream_name' => 'streamB'], 4, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388515')),
+                1 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 1, 'expected_position' => 1, 'expected_stream_name' => 'streamB'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388501')),
+                2 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 2, 'expected_position' => 2, 'expected_stream_name' => 'streamB'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388503')),
+                3 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 4, 'expected_position' => 3, 'expected_stream_name' => 'streamB'], 3, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388509')),
+                4 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 6, 'expected_position' => 4, 'expected_stream_name' => 'streamB'], 4, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388515')),
             ]),
             'streamC' => new InMemoryStreamIterator([
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 0, 'expected_stream_name' => 'streamC'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388500')),
-                TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 3, 'expected_stream_name' => 'streamC'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388503')),
+                1 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 0, 'expected_position' => 1, 'expected_stream_name' => 'streamC'], 1, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388500')),
+                2 => TestDomainEvent::withPayloadAndSpecifiedCreatedAt(['expected_index' => 3, 'expected_position' => 2, 'expected_stream_name' => 'streamC'], 2, DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s.u', '2019-05-10T10:18:19.388503')),
             ]),
         ];
     }
@@ -85,9 +85,36 @@ class MergedStreamIteratorTest extends AbstractStreamIteratorTest
     {
         $iterator = new MergedStreamIterator(\array_keys($this->getStreams()), ...\array_values($this->getStreams()));
 
-        foreach ($iterator as $key => $message) {
-            $this->assertEquals($key, $message->payload()['expected_index']);
+        $index = 0;
+        foreach ($iterator as $position => $message) {
+            $this->assertEquals($index, $message->payload()['expected_index']);
             $this->assertEquals($iterator->streamName(), $message->payload()['expected_stream_name']);
+
+            $index++;
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_correct_stream_name(): void
+    {
+        $iterator = new MergedStreamIterator(\array_keys($this->getStreams()), ...\array_values($this->getStreams()));
+
+        foreach ($iterator as $position => $message) {
+            $this->assertEquals($iterator->streamName(), $message->payload()['expected_stream_name']);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function key_represents_event_position(): void
+    {
+        $iterator = new MergedStreamIterator(\array_keys($this->getStreams()), ...\array_values($this->getStreams()));
+
+        foreach ($iterator as $position => $message) {
+            $this->assertEquals($position, $message->payload()['expected_position']);
         }
     }
 }
