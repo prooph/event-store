82613773727192a6b583ea9e4168012b3601ad6a
Chris Harrison <chris.harrison@funeralzone.co.uk>
2019-10-24 12:04:46 +0100

    Add a failing test to demonstrate the fact that subsequent messages raised by an upcaster are removed by the upcaster chain when calling further upcasters

--- tests/Upcasting/UpcasterChainTest.php
+++ tests/Upcasting/UpcasterChainTest.php
@@ -17,7 +17,9 @@ use PHPUnit\Framework\TestCase;
 use Prooph\Common\Messaging\Message;
 use Prooph\EventStore\Upcasting\NoOpEventUpcaster;
 use Prooph\EventStore\Upcasting\SingleEventUpcaster;
+use Prooph\EventStore\Upcasting\Upcaster;
 use Prooph\EventStore\Upcasting\UpcasterChain;
+use Prophecy\Argument;
 
 class UpcasterChainTest extends TestCase
 {
@@ -53,6 +55,60 @@ class UpcasterChainTest extends TestCase
         $this->assertNotEmpty($messages);
         $this->assertSame($upcastedMessage2, $messages[0]);
         $this->assertSame($upcastedMessage3, $messages[1]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_doesnt_remove_messages_when_a_subsequent_upcaster_returns_fewer_messages()
+    {
+        $initialMessage = $this->prophesize(Message::class);
+        $initialMessage->payload()->willReturn(['name' => 'initialMessage']);
+        $initialMessage = $initialMessage->reveal();
+
+        $subsequentMessage = $this->prophesize(Message::class);
+        $subsequentMessage->payload()->willReturn(['name' => 'subsequentMessage']);
+        $subsequentMessage = $subsequentMessage->reveal();
+
+        $modifiedSubsequentMessage = $this->prophesize(Message::class);
+        $modifiedSubsequentMessage->payload()->willReturn(['name' => 'subsequentMessage-modified']);
+        $modifiedSubsequentMessage = $modifiedSubsequentMessage->reveal();
+
+        $furtherSubsequentMessage = $this->prophesize(Message::class);
+        $furtherSubsequentMessage->payload()->willReturn(['name' => 'furtherSubsequentMessage']);
+        $furtherSubsequentMessage = $furtherSubsequentMessage->reveal();
+
+        $upcasterOne = $this->prophesize(Upcaster::class);
+        $upcasterOne->upcast($initialMessage)->willReturn([$initialMessage]);
+
+        $upcasterTwo = $this->prophesize(Upcaster::class);
+        $upcasterTwo->upcast($initialMessage)->willReturn([$initialMessage, $subsequentMessage]);
+
+        $upcasterThree = $this->prophesize(Upcaster::class);
+        $upcasterThree->upcast($initialMessage)->willReturn([$initialMessage]);
+        $upcasterThree->upcast($subsequentMessage)->willReturn([$modifiedSubsequentMessage, $furtherSubsequentMessage]);
+
+        $upcasterFour = new NoOpEventUpcaster();
+
+        $upcasterChain = new UpcasterChain(
+            $upcasterOne->reveal(),
+            $upcasterTwo->reveal(),
+            $upcasterThree->reveal(),
+            $upcasterFour
+        );
+
+        $messages = $upcasterChain->upcast($initialMessage);
+
+        $expectedMessagePayloads = [
+            ['name' => 'initialMessage'],
+            ['name' => 'subsequentMessage-modified'],
+            ['name' => 'furtherSubsequentMessage'],
+        ];
+        $messagePayloads = array_map(function (Message $message) {
+            return $message->payload();
+        }, $messages);
+
+        $this->assertEquals($expectedMessagePayloads, $messagePayloads);
     }
 
     protected function createUpcasterWhoCanUpcast(): SingleEventUpcaster
