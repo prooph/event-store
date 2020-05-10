dd83d3d36263bd82aa6b0673e004cb098a3e69d0
unixslayer <piotr.zajac@unixslayer.pl>
2020-05-10 22:01:10 +0200

    removing json strings projection state can be any payload based on query definition renaming ProjectionState to just State because it is also partition state changing Promise<string> return tag

---
+++ src/Projections/ProjectionStatistics.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of prooph/event-store.
+ * (c) 2014-2020 Alexander Miertsch <kontakt@codeliner.ws>
+ * (c) 2015-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore\Projections;
+
+final class ProjectionStatistics
+{
+    /** @var list<ProjectionDetails>  */
+    private array $projections;
+
+    /**
+     * @param list<ProjectionDetails> $projections
+     */
+    public function __construct(array $projections)
+    {
+        $this->projections = $projections;
+    }
+
+    /**
+     * @return list<ProjectionDetails>
+     */
+    public function projections(): array
+    {
+        return $this->projections;
+    }
+}
