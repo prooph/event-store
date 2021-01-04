6c8c1a381d332d0edcd94cff93abad49f5397bf4
Fritz Gerneth <gerneth.fritz@funct.com>
2021-01-04 23:46:01 +0100

    Update docheader

--- tests/ActionEventEmitterEventStoreTest.php
+++ tests/ActionEventEmitterEventStoreTest.php
@@ -2,8 +2,8 @@
 
 /**
  * This file is part of prooph/event-store.
- * (c) 2014-2020 prooph software GmbH <contact@prooph.de>
- * (c) 2015-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2014-2021 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2021 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace ProophTest\EventStore;
 
 use ArrayIterator;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Prooph\Common\Event\ActionEvent;
 use Prooph\Common\Event\ProophActionEventEmitter;
 use Prooph\EventStore\ActionEventEmitterEventStore;
@@ -26,6 +25,7 @@ use Prooph\EventStore\Metadata\MetadataMatcher;
 use Prooph\EventStore\Metadata\Operator;
 use Prooph\EventStore\StreamName;
 use ProophTest\EventStore\Mock\UsernameChanged;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 class ActionEventEmitterEventStoreTest extends ActionEventEmitterEventStoreTestCase
 {
